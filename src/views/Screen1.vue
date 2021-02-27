fb6b03502d0182b61cf38a6e4e1dfd7c772e2842
Alberto García <albertoinformatica97@gmail.com>
2021-02-28 00:39:32 +0100

    drag and drop to upload files

--- src/views/Screen1.vue
+++ src/views/Screen1.vue
@@ -1,19 +1,107 @@
 <template>
   <div class="h-full">
-    <div class="body-screen">
-      <div class="mx-4">
-        <label for="file1Name">File 1:</label>
-        <input type="text" id="file1Name" v-model="file1Name" />
-        <div class="drag-container">
-          <input type="file" @change="getFile1" name="" id="" />
-        </div>
+    <div class="body-screen space-x-4">
+      <div class="drag-drop-container">
+        <form
+          :class="{ 'is-dragover': isDragoverFile1 }"
+          method="post"
+          action=""
+          enctype="multipart/form-data"
+          @drag.stop.prevent
+          @dragstart.stop.prevent
+          @dragend.stop.prevent
+          @dragover.stop.prevent="drag('file1')"
+          @dragenter.stop.prevent="drag('file1')"
+          @dragleave.stop.prevent="dragLeave('file1')"
+          @drop.prevent="dropFile1"
+        >
+          <div>
+            <span
+              class="absolute top-5 left-5 font-light tracking-wider opacity-75"
+            >
+              File 1
+            </span>
+            <input
+              type="file"
+              name="files[]"
+              ref="file1"
+              id="file1"
+              @change="getFile1"
+            />
+            <div>
+              <img
+                v-if="!file1Chosen"
+                src="@/assets/images/upload.svg"
+                alt="upload"
+                class="inline-block m-auto w-36 h-36 opacity-50"
+              />
+              <img
+                v-else
+                src="@/assets/images/done.svg"
+                alt="uploaded"
+                class="inline-block m-auto w-36 h-36 opacity-50"
+              />
+            </div>
+            <label for="file1" v-if="!file1Chosen">
+              <strong class="cursor-pointer hover:text-gray-600">
+                Choose a file
+              </strong>
+              <span class="inline"> or drag it here</span>.
+            </label>
+            <span v-else>{{ file1Chosen.name }}</span>
+          </div>
+        </form>
       </div>
-      <div class="mx-4">
-        <label for="file2Name">File 2:</label>
-        <input type="text" id="file2Name" v-model="file2Name" />
-        <div class="drag-container">
-          <input type="file" @change="getFile2" name="" id="" />
-        </div>
+      <div class="drag-drop-container">
+        <form
+          :class="{ 'is-dragover': isDragoverFile2 }"
+          method="post"
+          action=""
+          enctype="multipart/form-data"
+          @drag.stop.prevent
+          @dragstart.stop.prevent
+          @dragend.stop.prevent
+          @dragover.stop.prevent="drag('file2')"
+          @dragenter.stop.prevent="drag('file2')"
+          @dragleave.stop.prevent="dragLeave('file2')"
+          @drop.prevent="dropFile2"
+        >
+          <div>
+            <span
+              class="absolute top-5 left-5 font-light tracking-wider opacity-75"
+            >
+              File 2
+            </span>
+            <input
+              type="file"
+              name="files[]"
+              ref="file2"
+              id="file2"
+              @change="getFile2"
+            />
+            <div>
+              <img
+                v-if="!file2Chosen"
+                src="@/assets/images/upload.svg"
+                alt="upload"
+                class="inline-block m-auto w-36 h-36 opacity-50"
+              />
+              <img
+                v-else
+                src="@/assets/images/done.svg"
+                alt="uploaded"
+                class="inline-block m-auto w-36 h-36 opacity-50"
+              />
+            </div>
+            <label for="file2" v-if="!file2Chosen">
+              <strong class="cursor-pointer hover:text-gray-600">
+                Choose a file
+              </strong>
+              <span class="inline"> or drag it here</span>.
+            </label>
+            <span v-else>{{ file2Chosen.name }}</span>
+          </div>
+        </form>
       </div>
     </div>
     <footer-control
@@ -35,8 +123,10 @@ export default {
     return {
       file1Name: 'f1',
       file2Name: 'f2',
-      file1Chosen: {},
-      file2Chosen: {}
+      file1Chosen: null,
+      file2Chosen: null,
+      isDragoverFile1: false,
+      isDragoverFile2: false
     }
   },
   methods: {
@@ -63,6 +153,32 @@ export default {
 
       return true
     },
+    drag(file) {
+      if (file === 'file1') {
+        this.isDragoverFile1 = true
+      } else {
+        this.isDragoverFile2 = true
+      }
+    },
+    dragLeave(file) {
+      if (file === 'file1') {
+        this.isDragoverFile1 = false
+      } else {
+        this.isDragoverFile2 = false
+      }
+    },
+    dropFile1(e) {
+      this.dragLeave('file1')
+      this.$refs.file1.files = e.dataTransfer.files
+      this.file1Chosen = [...this.$refs.file1.files][0]
+      console.log([...this.$refs.file1.files][0])
+    },
+    dropFile2(e) {
+      this.dragLeave('file2')
+      this.$refs.file2.files = e.dataTransfer.files
+      this.file2Chosen = [...this.$refs.file2.files][0]
+      console.log([...this.$refs.file2.files][0])
+    },
     nextScreen() {
       this.buildFilesObjects()
       if (this.validateFiles()) {
@@ -85,7 +201,39 @@ export default {
 </script>
 
 <style>
-.drag-container {
-  @apply py-20 px-24 border border-gray-700 border-dashed;
+.drag-drop-container {
+  @apply w-2/5 relative;
+}
+
+form {
+  @apply bg-yellow-300 p-12;
+  color: #0f3c4b;
+  text-align: center;
+  vertical-align: baseline;
+  outline: 2px dashed #92b0b3;
+  outline-offset: -10px;
+  transition: outline-offset 0.15s ease-in-out, background-color 0.15s linear;
+}
+
+form input[type='file'] {
+  display: none;
+  /* vertical-align: baseline;
+  padding: 0;
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  margin: 0;
+  line-height: normal;
+  border-radius: 0;
+  border: none;
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1; */
+}
+
+.is-dragover {
+  @apply bg-white;
 }
 </style>
